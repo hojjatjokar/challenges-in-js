8223190ff6512ad5b16a22d871dbd08b7728fb6d
Hojjat Jokar <paintthedream@gmail.com>
2021-02-24 13:04:52 +0330

    feat(valid-anagram): add empty and edge case validation

--- src/contains-duplicate/contains-duplicate.test.js
+++ src/contains-duplicate/contains-duplicate.test.js
@@ -1,11 +1,29 @@
-import containsDuplicate from './index';
+import containsDuplicate, {errorMessages} from './index';
 
 test('Does [1,2,3,1] contain duplicate', () => {
   expect(containsDuplicate([1,2,3,1])).toBe(true);
 });
+
 test('Does [1,1,1,3,3,4,3,2,4,2] contain duplicate', () => {
   expect(containsDuplicate([1,1,1,3,3,4,3,2,4,2])).toBe(true);
 });
+
 test('Does [1,2,3,4] contain duplicate', () => {
   expect(containsDuplicate([1,2,3,4])).toBe(false);
+});
+
+test('Does throw proper error for empty argument', () => {
+  expect(() => containsDuplicate()).toThrow(errorMessages.argumentType);
+});
+
+test('Does throw proper error for wrong type of argument', () => {
+  expect(() => containsDuplicate('123456')).toThrow(errorMessages.argumentType);
+});
+
+test('Does throw proper error for wrong type of content of argument', () => {
+  expect(() => containsDuplicate([1,2,6,'6'])).toThrow(errorMessages.argumentContent);
+});
+
+test('Does throw proper error for wrong type of content of argument', () => {
+  expect(() => containsDuplicate([NaN])).toThrow(errorMessages.argumentContent);
 });
--- src/contains-duplicate/index.js
+++ src/contains-duplicate/index.js
@@ -1,11 +1,17 @@
+const errorMessages = {
+  argumentType: 'ContainsDuplicate function expect the argument to be a type of array',
+  argumentContent: 'ContainsDuplicate function expect the argument to be a array of numers, your array is likely contain a type of other than number',
+}
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 function containsDuplicate(nums){
+  if(!Array.isArray(nums)) throw new Error(errorMessages.argumentType);
   const numsMap = new Map();
 
   for(const item of nums){
+    if(typeof item !== 'number' || Number.isNaN(item)) throw Error(errorMessages.argumentContent);
     if(numsMap.has(item)) return true;
     numsMap.set(item, true);
   }
@@ -13,15 +19,7 @@ function containsDuplicate(nums){
   return false;
 }
 
+export {
+  errorMessages,
+}
 export default containsDuplicate;
-
-// function containsDuplicate(nums) {
-//   const hashMap = new Map();
-
-//   for (const value of nums) {
-//     if (hashMap.has(value)) return true;
-//     hashMap.set(value, true);
-//   }
-
-//   return false;
-// }
